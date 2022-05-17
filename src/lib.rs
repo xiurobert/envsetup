886309af22b6eb5e3b7471943e68910c24174300
Robert Xiu <xiurobert@gmail.com>
2022-05-17 22:36:48 +0800

    docs: add light documentation for existing functions

--- src/lib.rs
+++ src/lib.rs
@@ -11,12 +11,17 @@ pub struct EnvSetupConfig {
     setup_cmds: Vec<String>,
 }
 
+/// Ingests a configuration file and returns a `EnvSetupConfig` struct
+/// The configuration file should be a YAML file. This function will throw an error if
+/// the file could not be deserialized into the EnvSetupConfig struct
 pub fn ingest_configuration_file(config_path: &str) -> Result<EnvSetupConfig, Box<dyn Error>> {
     let conf_str = fs::read_to_string(config_path)?;
     let result: EnvSetupConfig = serde_yaml::from_str(&conf_str)?;
     Ok(result)
 }
 
+/// Validates the language parameter in the configuration file and ensures that it is supported
+/// by this tool
 fn validate_language(language: &str) -> bool {
     match language {
         "rust" => {
@@ -35,7 +40,12 @@ fn validate_language(language: &str) -> bool {
     }
 }
 
+/// Validates the git configuration in the configuration file and ensures that the git
+/// configuration complies with the specification and is supported by this program
 fn validate_git_conf(git_conf: &HashMap<String, String>) -> bool {
+    if git_conf.is_empty() {
+        return false;
+    }
     let accepted_values = [String::from("repo"), String::from("branch")];
     let mut result = true;
     for key in git_conf.keys() {
@@ -46,6 +56,7 @@ fn validate_git_conf(git_conf: &HashMap<String, String>) -> bool {
     result
 }
 
+/// The main run function. Called from the main.rs file.
 pub fn run(conf_path: &str) {
     let e_config = ingest_configuration_file(conf_path).unwrap_or_else(|e| {
         panic!("Error: {}", e);
@@ -53,6 +64,7 @@ pub fn run(conf_path: &str) {
     process_config(&e_config);
 }
 
+/// Processes the configuration object and runs appropriate checks and commands
 fn process_config(conf: &EnvSetupConfig) {
     let language = &conf.language;
     let git_conf = &conf.git;
@@ -63,8 +75,8 @@ fn process_config(conf: &EnvSetupConfig) {
         return;
     }
 
-    if git_conf.is_empty() {
-        println!("No git configuration found");
+    if !validate_git_conf(&conf.git) {
+        println!("Git configuration is invalid");
         return;
     }
 
@@ -73,10 +85,11 @@ fn process_config(conf: &EnvSetupConfig) {
         return;
     }
 
-    let git_cmds = process_git_cmds(git_conf);
+    let git_cmd_results = execute_cmd_list(&process_git_cmds(git_conf));
 }
 
-fn execute_cmd_list(cmd_list: &Vec<String>) -> Vec<bool> {
+/// Execute a list of commands
+fn execute_cmd_list(cmd_list: &[String]) -> Vec<bool> {
     let mut results = Vec::new();
     for cmd in cmd_list.iter() {
         results.push(execute_terminal_command(cmd));
@@ -84,6 +97,9 @@ fn execute_cmd_list(cmd_list: &Vec<String>) -> Vec<bool> {
     results
 }
 
+/// Execute a command "in the terminal"
+/// This function tries to emulate the experience of typing the command in your terminal
+/// and pressing enter.
 fn execute_terminal_command(cmd: &str) -> bool {
     let output = if cfg!(target_os = "windows") {
         Command::new("cmd").arg("/C").arg(cmd).output()
@@ -96,6 +112,9 @@ fn execute_terminal_command(cmd: &str) -> bool {
     }
 }
 
+/// Processes the git commands as defined in the envsetup.yml configuration file
+/// And dumps them out as a list of console-executable commands
+/// Silently discards any commands that are not supported
 fn process_git_cmds(git_conf: &HashMap<String, String>) -> Vec<String> {
     let mut git_cmds = Vec::new();
     for (key, value) in git_conf {
@@ -114,6 +133,7 @@ fn process_git_cmds(git_conf: &HashMap<String, String>) -> Vec<String> {
     git_cmds
 }
 
+/// Checks if rustup is installed
 fn ensure_rustup_present() -> bool {
     if cfg!(target_os = "windows") {
         execute_terminal_command("where rustup")
