98c03fba72df058027f2b1cfc4de5b807e1c4440
Robert <xiurobert@gmail.com>
2022-05-18 16:07:28 +0800

    feat: add subcommands

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,32 @@
 use envsetup::run;
 
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[clap(author, version, about)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Option<Commands>
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Here,
+    Get {
+        repo: String
+    }
+}
+
 fn main() {
     println!("envsetup v{}", env!("CARGO_PKG_VERSION"));
+    let cli = Cli::parse();
+    match &cli.command {
+        Some(Commands::Get { repo }) => println!("Repo: {}", repo),
+        Some(Commands::Here) => run("envsetup.yml"),
+        None => {
+            println!("No command specified");
+
+        }
+    }
     // todo: support for specifying a github repo, and pulling the information from there
-    run("envsetup.yml");
 }
