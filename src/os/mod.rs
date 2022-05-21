4242ab6602e1d71cac5db091fbf94b7a6ac12402
Robert <xiurobert@gmail.com>
2022-05-21 22:19:52 +0800

    feat: start work on auto installation of tools

---
+++ src/os/mod.rs
@@ -0,0 +1,14 @@
+use std::env;
+use crate::ensure_tool_present;
+
+pub fn check_homebrew_present() -> bool {
+    cfg!(target_os = "macos") && ensure_tool_present("brew")
+}
+
+pub fn check_aptget_present() -> bool {
+    cfg!(target_os = "linux") && ensure_tool_present("apt-get")
+}
+
+pub fn check_choco_present() -> bool {
+    cfg!(target_os = "windows") && ensure_tool_present("choco")
+}
