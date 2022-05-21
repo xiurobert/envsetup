a7af776c56cc74dfff440cb606eb864385beaa38
Robert <xiurobert@gmail.com>
2022-05-21 16:46:41 +0800

    docs: improve documentation when executing
    This improves the documentation when running the CLI command

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,27 @@ use clap::{Parser, Subcommand};
 struct Cli {
     #[clap(subcommand)]
     command: Option<Commands>,
+
+    /// Whether to automatically install all the necessary components on your system
+    /// if they are not present
+    #[clap(short, long)]
+    auto_install: bool
 }
 
 #[derive(Subcommand)]
 enum Commands {
+    /// Runs envsetup in this folder. Looks for envsetup.yml to perform the setup
+    /// tasks
     Here,
-    Get { repo: String },
+    /// Pulls a repo from GitHub and automatically runs envsetup in the project
+    Get {
+        /// The repo to pull. Should be in the format username/repo (e.g. rust-lang/rust)
+        repo: String
+    },
 }
 
 fn main() {
-    println!("envsetup v{}", env!("CARGO_PKG_VERSION"));
+    //println!("envsetup v{}", env!("CARGO_PKG_VERSION"));
     // todo: move this into lib.rs
     let cli = Cli::parse();
     match &cli.command {
